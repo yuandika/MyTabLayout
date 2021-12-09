34c5c3855d42d694a2088756bc730ef1f79a7c62
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-12-09 20:22:52 +0700

    init commit

---
+++ app/src/main/java/com/dicoding/mytablayout/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.dicoding.mytablayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLE[position])
+        }.attach()
+
+        supportActionBar?.elevation = 0f
+    }
+
+    companion object {
+        @StringRes
+        private val TAB_TITLE = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2
+        )
+    }
+}
